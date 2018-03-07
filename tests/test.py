5dacf4874f5e9fcad5acc4c9a98533035b6637bf
ipselium <ipselium@free.fr>
2018-03-07 03:08:07 +0100

    modified:   tests/test.py

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@
 """
 File Name : test.py
 Creation Date : mar. 06 mars 2018 17:27:32 CET
-Last Modified : mar. 06 mars 2018 23:34:56 CET
+Last Modified : mer. 07 mars 2018 00:15:28 CET
 Created By : Cyril Desjouy
 
 Copyright © 2016-2018 Cyril Desjouy <ipselium@free.fr>
@@ -15,15 +15,21 @@ DESCRIPTION
 @author: Cyril Desjouy
 """
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 a = 1
 b = 3
 
-é = 1
-print(a)
-
-a = [i for i in range(12) if i % 2 == 0]
+a = [i for i in range(11) if i % 2 == 0]
 print(a)
 
 for i in range(10):
     i = 3*i
     print(i)
+
+x = np.linspace(0, 2*np.pi, 1000)
+
+plt.figure()
+plt.plot(x, np.cos(x))
+plt.show()
